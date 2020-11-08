0fff2ade85b484b1ab49d95facad9c1dbd73fb27
akhisyabab <akhisyabab@gmail.com>
2020-11-08 22:22:13 +0700

    read from text modification

--- text-read-from-file.py
+++ text-read-from-file.py
@@ -4,6 +4,10 @@ list_data = [line.replace('\n', '') for line in f.readlines() if line != '\n']
 print(list_data)
 
 
+# method 2
+list_data = [line.rstrip('\n') for line in open('text_file.txt')]
+
+
 
 
 
