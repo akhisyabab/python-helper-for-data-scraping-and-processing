326e0031a877887367cb3b67b97700762130219c
akhisyabab <akhisyabab@gmail.com>
2021-05-18 16:48:44 +0700

    headers

---
+++ headers_requests.py
@@ -0,0 +1,18 @@
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': 'cl_b=4|139851b4d2454f346bdbe0f8ef827e1a9e865872|1621261987X7LX8; cl_def_hp=sfbay; cl_tocmode=ccc%3Alist%2Cjjj%3Alist%2Csss%3Agrid',
+    'Host': 'sfbay.craigslist.org',
+    'If-Modified-Since': 'Mon, 17 May 2021 11:13:38 GMT',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'sec-ch-ua-mobile': '?0',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+} 
